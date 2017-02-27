04aadbf101803ce0dea71697c1305d478bd18cd7
Kalgarn <kalgarn@tuta.io>
2017-02-27 10:57:51 +0100

    some code

--- desktop/src/kalgarn/tetrisgdx/desktop/DesktopLauncher.java
+++ desktop/src/kalgarn/tetrisgdx/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import kalgarn.tetrisgdx.TetrisGdx;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Tetris";
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new TetrisGdx(), config);
 	}
 }
